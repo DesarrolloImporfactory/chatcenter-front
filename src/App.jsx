ef479dcd716fe73a7bd9fe6fd4c43cc7362eba07
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-23 10:35:55 -0500

    XD

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import PostId from "./pages/postId/PostId";
-import ProtectedRoutes from "./pages/shared/ProtectedRoutes";
+
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { getAllPostThunk } from "./store/slices/posts.slice";
-import { getMyPostsThunk } from "./store/slices/myPosts.slice";
+
 import Header from "./components/shared/Header";
-import { UserId } from "./pages/userId/UserId";
-import { Profile } from "./pages/profile/Profile";
+
 import { Footer } from "./components/shared/Footer";
 import { renewThunk } from "./store/slices/user.slice";
 
