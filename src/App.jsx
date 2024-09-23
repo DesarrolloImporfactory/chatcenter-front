675cca2906de216671b285c8feec6608661d40a7
Desarrollo2Imporfactory <desarrollo2imporfactory@gmail.com>
2024-09-23 10:57:12 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
          */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/registro" element={<Register />} />
         <Route path="*" element={<h1>Esta ruta no existe</h1>} />
       </Routes>
       <Footer />
