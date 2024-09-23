26cf3176cb7e1400b19266b97260682339570f01
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-23 10:36:51 -0500

    XD

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import posts from "./slices/posts.slice";
-import myPosts from "./slices/myPosts.slice";
+
 import user from "./slices/user.slice";
 
 export default configureStore({
   reducer: {
-    posts,
-    myPosts,
     user,
   },
 });
