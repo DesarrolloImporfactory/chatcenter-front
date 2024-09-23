37235f7ecea305b19ad5cc6f3645e24a6e727178
Jeimy Jara <jeimyjara@hotmail.com>
2024-09-23 10:39:00 -0500

    XD

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,22 @@
 import PostCard from "../../components/home/PostCard";
 import "./home.css";
 import { useDispatch, useSelector } from "react-redux";
-import FormPost from "../../components/home/FormPost";
-import { useState, useEffect } from "react";
-import { useSocket } from "../../hooks/useSocket";
-import { addPost } from "./../../store/slices/posts.slice";
+
 /* ----  */
 
 const Home = () => {
-  const { posts } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   // sino detecta token, redirige a login
 
-  const [isCloseForm, setIsCloseForm] = useState(true);
-  const { socket, online } = useSocket("http://localhost:3000");
-
-  useEffect(() => {
+  /* useEffect(() => {
     console.log(posts);
     socket.on("render-new-post", (data) => {
       dispatch(addPost(data));
       console.log("Se ha creado un post", data);
     });
   }, [socket]);
-
+ */
   const handleCreatePost = () => {
     setIsCloseForm(false);
   };
@@ -38,19 +31,10 @@ const Home = () => {
         will guide you through a fascinating intellectual journey. Join us in
         the quest for endless knowledge.
       </p>
-      <div className="home__post-container">
-        {posts?.posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
-      </div>
+      <div className="home__post-container"></div>
       <button onClick={handleCreatePost} className="home__btn">
         +
       </button>
-      <FormPost
-        isCloseForm={isCloseForm}
-        setIsCloseForm={setIsCloseForm}
-        socket={socket}
-      />
     </div>
   );
 };
